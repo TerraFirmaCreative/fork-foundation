d5fc636c5bc7f7ca949114a5b39ecabb253b823a
tipex360 <tymsusz@gmail.com>
2026-03-10 13:06:46 +0100

    improve formatPrice util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,5 +6,5 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formatPrice(price: { amount: string, currencyCode: string }) {
-  return `${new Intl.NumberFormat(undefined, { style: "currency", currency: price.currencyCode, currencyDisplay: "narrowSymbol", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(parseFloat(price.amount))} ${price.currencyCode}`
+  return `${new Intl.NumberFormat(undefined, { style: "currency", currency: price.currencyCode, currencyDisplay: "narrowSymbol", minimumFractionDigits: 0, maximumFractionDigits: 0 }).format(parseFloat(price.amount))} ${price.currencyCode.endsWith('D') ? price.currencyCode : ""}`
 }
